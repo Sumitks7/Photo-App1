1d05cdb0d928dd32d1a0c3ca66480b284548c93a
Sumit Kumar <sumitkumar10011999@gmail.com>
2022-08-16 10:24:08 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Photo from "./components/Photos/Photo";
+import About from "./components/About"
+import Contact from "./components/Contact"
+import EnlargedPhoto from './components/EnlargedPhoto'
+import NotFound from './components/NotFound'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+				<Routes>
+					<Route path='/' element={<Photo />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/contact' element={<Contact />} />
+					<Route path='/photos/:id' element={<EnlargedPhoto />} />
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
+
